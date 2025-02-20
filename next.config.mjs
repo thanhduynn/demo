42298aed4cb134adceb48bb0613748cc974f15f0
asley <asley>
2025-02-20 22:20:20 +0700

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint:{
+        ignoreDuringBuilds: true,
+    },
+    output: 'export'
+};
 
 export default nextConfig;
